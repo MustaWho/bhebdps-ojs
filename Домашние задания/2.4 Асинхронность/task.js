4a12a7416fc9dcd4fe59b5eec97ee25e64783a02
MustaWho <fiq2000fiq@gmail.com>
2026-09-01 02:55:59 +0300

    HW-3-complete

---
+++ Домашние задания/2.4 Асинхронность/task.js
@@ -0,0 +1,72 @@
+class AlarmClock {
+  constructor() {
+    this.alarmCollection = [];
+    this.intervalId = null;
+  }
+
+  addClock(time, callback) {
+    if (!time || !callback) {
+      throw new Error('Отсутствуют обязательные аргументы');
+    }
+
+    let hasClock = this.alarmCollection.some(function(alarm) {
+      return alarm.time === time;
+    });
+
+    if (hasClock) {
+      console.warn('Уже присутствует звонок на это же время');
+    }
+
+    this.alarmCollection.push({
+      time,
+      callback,
+      canCall: true
+    });
+  }
+
+  removeClock(time) {
+    this.alarmCollection = this.alarmCollection.filter(function(alarm) {
+      return alarm.time !== time;
+    });
+  }
+
+  getCurrentFormattedTime() {
+    return new Date().toLocaleTimeString("ru-Ru", {
+      hour: "2-digit",
+      minute: "2-digit"
+    });
+  }
+
+  start() {
+    if (this.intervalId !== null) {
+      return;
+    }
+
+    this.intervalId = setInterval(() => {
+      let currentTime = this.getCurrentFormattedTime();
+
+      this.alarmCollection.forEach(function(alarm) {
+        if (alarm.time === currentTime && alarm.canCall) {
+          alarm.canCall = false;
+          alarm.callback();
+        }
+      });
+    }, 1000);
+  }
+
+  stop() {
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+  }
+
+  resetAllCalls() {
+    this.alarmCollection.forEach(function(alarm) {
+      alarm.canCall = true;
+    });
+  }
+
+  clearAlarms() {
+    this.stop();
+    this.alarmCollection = [];
+  }
+}
