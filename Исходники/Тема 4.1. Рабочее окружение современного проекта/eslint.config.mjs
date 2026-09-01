647306214df52af0e20553d4f246c151578286ab
MustaWho <fiq2000fiq@gmail.com>
2026-09-01 03:07:56 +0300

    HW-8-complete

--- Исходники/Тема 4.1. Рабочее окружение современного проекта/eslint.config.mjs
+++ Исходники/Тема 4.1. Рабочее окружение современного проекта/eslint.config.mjs
@@ -1,15 +1,29 @@
-import js from "@eslint/js";
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+import globals from 'globals';
+
+const compat = new FlatCompat({
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
 
 export default [
-    js.configs.recommended,
-   {
-    plugins: {
-      extends: ["airbnb-base/legacy"]
+  {
+    ignores: [
+      'dist/**',
+      'eslint.config.mjs',
+    ],
+  },
+  js.configs.recommended,
+  ...compat.extends('airbnb-base'),
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
-    rules: {
-      "no-undef": [
-        "error",
-      ]
-    }
-   }
+  },
 ];
